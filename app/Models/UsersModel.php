55a3ab9d720353104bf9d18d6a003eff10d656b7
bgnacinski <36711164+bgnacinski@users.noreply.github.com>
2022-12-28 16:52:37 +0100

    Users (#1)
    * added user migration
    
    * added user seeder
    
    * added user entity
    
    * made phone_number unique
    
    * added model for users

---
+++ app/Models/UsersModel.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UsersModel extends Model
+{
+    protected $table            = 'users';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ["email_address", "password", "phone_number", "auth_key", "permission_level"];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'int';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = ["hashPassword", "generateAuthKey"];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+
+    public function hashPassword(array $data) :array{
+        $data["data"]["password"] = password_hash($data["data"]["password"], PASSWORD_DEFAULT);
+        
+        return $data;
+    }
+
+    public function generateAuthKey(array $data) :array{
+        $auth_key = bin2hex(random_bytes(256));
+
+        $data["data"]["auth_key"] = $auth_key;
+
+        return $data;
+    }
+}
